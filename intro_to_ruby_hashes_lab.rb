7ddd648867b9cfa39b060979bdc3c36d933e74dc
Derric Fragosa <fderric@gmail.com>
2019-09-24 01:32:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 
 def monopoly_with_third_tier
  base = {:railroads =>{:pieces =>4, :rent_in_dollars =>
-        {:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :key4 =>{}},
+        {:one_piece_owned =>25, :two_pieces_owned =>50, :three_pieces_owned =>100, :four_pieces_owned =>200},
           :names =>{:name1 =>{}, :name2 =>{}, :name3 =>{}, 
           :name4 =>{} }}
    }
